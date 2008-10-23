5138f80dbfcbc2466f9bfea65c6420b0206056f8
Andreas Gohr <gohr@cosmocode.de>
2008-10-23 12:32:06 +0200

    Language updates
    darcs-hash:20081023103206-6e07b-2f8b826c351c653e0c85c52ecb5e094ffd279fba.gz

---
+++ lang/de/lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * german language file
+ */
+
+$lang['menu'] = 'Historie aufräumen';
+
+// custom language strings for the plugin
+$lang['name'] = 'Historie aufräumen';
+$lang['clean on recent changes'] = 'Das Wiki anhand der letzten Änderungen aufräumen.';
+$lang['clean on namespace'] = 'Einen Namensbereich aufräumen. (Achtung, dies kann unter Umständen länger dauern)';
+$lang['do'] = 'Historie aufrämen';
+$lang['deleted'] = '%d Einträge gelöscht';
+$lang['desc'] = 'Beschreibung';
+$lang['desctext'] = 'Dieses Plugin hilft ihnen, die Änderungshistorie ihres Wikis sauber zu halten.<br /><br />
+Das Plugin löscht alte Zwischenversionen die von nur einem Benutzer bearbeitet
+wurden, wenn die einzelnen Änderungen in einem Abstand von weniger als einer Stunde durchgeführt wurden.<br /><br />
+<b>Aufräumen anhand der letzten Änderungen:</b><br />
+Wenn diese Option ausgewählt ist, werden nur die Seiten welche in der Liste der
+letzten Änderungen stehen gesäubert.<br /><br />
+
+<b>Aufräumen nach Namensbereichen</b><br />
+Ist diese Option ausgewählt, wird der gegebene Namensbereich und alle darunter liegenden
+Namensbereiche aufgeräumt. Wenn kein Namensbereich angegeben ist, werden alle Seiten des gesamten Wikis untersucht.<br />
+Diese Option ist mit Vorsicht zu genießen, da sie unter Umständen eine lange Zeit benötigt.';
+
+?>
---
+++ lang/de/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['autoclearenabled'] = 'Die Historie automatisch einmal am Tag aufräumen.';
+
+?>
