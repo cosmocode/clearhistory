cfbdd1c77c688a3ed2dd72be8ba71eda4a525639
sawachan <chuno@live.jp>
2014-08-05 22:30:18 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+ 
+$lang['autoclearenabled']       = '一日一回、自動的に履歴を整理する。';
+$lang['autoclearonlysmall']     = '小変更のみ削除する。';
+$lang['autoclearonlynocomment'] = '「編集の概要」のない変更のみ削除する。';
