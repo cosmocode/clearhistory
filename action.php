e8571eb7d31a35f4109a95fc4fbf3d81f0ef7bd6
Andreas Gohr <andi@splitbrain.org>
2023-01-31 08:57:39 +0100

    formatting and php style cleanup

--- action.php
+++ action.php
@@ -1,85 +1,67 @@
 <?php
-if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-if(!defined('DOKU_DATA')) define('DOKU_DATA',DOKU_INC.'data/');
-
-require_once(DOKU_PLUGIN.'action.php');
-require_once(DOKU_PLUGIN.'clearhistory/admin.php');
 
 /**
  * Cleanup Action Plugin:   Clean up the history once per day
  *
  * @author     Dominik Eckelmann <eckelmann@cosmocode.de>
  */
-class action_plugin_clearhistory extends DokuWiki_Action_Plugin {
+class action_plugin_clearhistory extends DokuWiki_Action_Plugin
+{
 
-	/**
-	 * if true a cleanup process is already running
-	 * or done in the last 24h
-	 */
-    var $run = false;
+    /**
+     * if true a cleanup process is already running
+     * or done in the last 24h
+     */
+    protected $run = false;
 
-	/**
-	 * Constructor - get some config details and check if a check runs in the last 24h
-	 */
-    function action_plugin_clearhistory() {
+    /**
+     * Constructor - get some config details and check if a check runs in the last 24h
+     */
+    public function __construct()
+    {
         global $conf;
 
-		// check if the autocleaner is enabled
+        // check if the autocleaner is enabled
         if ($this->getConf('autoclearenabled') == 0) $this->run = true;
 
-		// check if a runfile exists - if not -> there is no last run
-        if (!is_file($conf['cachedir'].'/lastclean')) return;
+        // check if a runfile exists - if not -> there is no last run
+        if (!is_file($conf['cachedir'] . '/lastclean')) return;
 
-		// check last run
-        $get = fileatime($conf['cachedir'].'/lastclean');
+        // check last run
+        $get = fileatime($conf['cachedir'] . '/lastclean');
         $get = intval($get);
-        if ($get+(60*60*24) > time()) $this->run = true;
+        if ($get + (60 * 60 * 24) > time()) $this->run = true;
+    }
+
+    /** @inheritdoc */
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('INDEXER_TASKS_RUN', 'BEFORE', $this, 'cleanup', array());
     }
 
     /**
-	 * return some in
-	 * @return array
+     * start the scan
+     *
+     * scans the recent changes
      */
-    function getInfo(){
-        return confToHash(dirname(__FILE__).'/plugin.info.txt');
-    }
-
-    /**
-     * Register its handlers with the dokuwiki's event controller
-	 *
-	 * we need hook the indexer to trigger the cleanup
-     */
-    function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('INDEXER_TASKS_RUN', 'BEFORE',  $this, 'cleanup', array());
-    }
-
-	/**
-	 * start the scan
-	 *
-	 * scans the recent changes
-	 */
-    function cleanup(&$event, $param) {
+    public function cleanup($event, $param)
+    {
         global $conf;
 
-		if ($this->run) return;
-		$this->run = true;
-		echo 'clearhistory: started'.NL;
-		
-		$onlySmall     = $this->getConf('autoclearonlysmall');
-        $onlyNoComment = $this->getConf('autoclearonlynocomment');        
-		
+        if ($this->run) return;
+        $this->run = true;
+        echo 'clearhistory: started' . NL;
+
+        $onlySmall = $this->getConf('autoclearonlysmall');
+        $onlyNoComment = $this->getConf('autoclearonlynocomment');
+
         //$hdl = plugin_load('admin','clearhistory');
         $hdl = new admin_plugin_clearhistory();
-        
-		$hdl->_scanRecents(30, $onlySmall , $onlyNoComment);
 
-		echo 'clearhistory: ' . $hdl->delcounter . ' deleted'.NL;
-        touch($conf['cachedir'].'/lastclean');
+        $hdl->_scanRecents(30, $onlySmall, $onlyNoComment);
+
+        echo 'clearhistory: ' . $hdl->delcounter . ' deleted' . NL;
+        touch($conf['cachedir'] . '/lastclean');
     }
 
 }
-
-
-
-?>
--- admin.php
+++ admin.php
@@ -1,12 +1,4 @@
 <?php
-if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once(DOKU_PLUGIN.'admin.php');
-require_once(DOKU_INC.'inc/pageutils.php');
-require_once(DOKU_INC.'inc/changelog.php');
-require_once(DOKU_INC.'inc/io.php');
-require_once(DOKU_INC.'inc/common.php');
 
 /**
  * This plugin is used to cleanup the history
@@ -14,7 +6,8 @@ require_once(DOKU_INC.'inc/common.php');
  * @see    http://dokuwiki.org/plugin:clearhistory
  * @author Dominik Eckelmann <deckelmann@gmail.com>
  */
-class admin_plugin_clearhistory extends DokuWiki_Admin_Plugin {
+class admin_plugin_clearhistory extends DokuWiki_Admin_Plugin
+{
 
     /**
      * pages in a hiracical view
@@ -22,102 +15,89 @@ class admin_plugin_clearhistory extends DokuWiki_Admin_Plugin {
      * "<namespace>" => array( ... )
      * "pages" => array ( pagenames )
      */
-    var $pages = array();
-
-	/**
-	 * counts deleted pages for a run
-	 */
-    var $delcounter = 0;
+    protected $pages = array();
 
     /**
-     * return some information about the plugin
-	 *
-	 * @return array
+     * counts deleted pages for a run
      */
-    function getInfo(){
-      return confToHash(dirname(__FILE__).'/plugin.info.txt');
-    }
-
-    /**
-     * return sort order for position in admin menu
-	 *
-	 * @return integer
-     */
-    function getMenuSort() {
-      return 999;
-    }
+    public $delcounter = 0;
 
     /**
      * handle the request befor html output
-	 *
-	 * @see html()
+     *
+     * @see html()
      */
-    function handle() {
+    public function handle()
+    {
         $onlySmall = false;
         $onlsNoComment = false;
-        if (isset($_REQUEST['onlysmall']) && $_REQUEST['onlysmall'] == 'on' ) $onlySmall = true;
-        if (isset($_REQUEST['onlynocomment']) && $_REQUEST['onlynocomment'] == 'on' ) $onlyNoComment = true;
+        if (isset($_REQUEST['onlysmall']) && $_REQUEST['onlysmall'] == 'on') $onlySmall = true;
+        if (isset($_REQUEST['onlynocomment']) && $_REQUEST['onlynocomment'] == 'on') $onlyNoComment = true;
 
         if (isset($_GET['clear'])) {
             if ($_GET['clear'] == 1) {
-                $this->_scanRecents( 30 , $onlySmall , $onlyNoComment );
-            } else if ($_GET['clear'] == 2) {
-                $_GET['ns'] = cleanID($_GET['ns']);
-                $this->_scanNamespace($_GET['ns'] , $onlySmall , $onlyNoComment );
+                $this->_scanRecents(30, $onlySmall, $onlyNoComment);
+            } else {
+                if ($_GET['clear'] == 2) {
+                    $_GET['ns'] = cleanID($_GET['ns']);
+                    $this->_scanNamespace($_GET['ns'], $onlySmall, $onlyNoComment);
+                }
             }
-            msg(sprintf($this->getLang('deleted'),$this->delcounter),1);
+            msg(sprintf($this->getLang('deleted'), $this->delcounter), 1);
         }
     }
 
     /**
      * output html for the admin page
      */
-    function html() {
-        echo '<h1>'.$this->getLang('name').'</h1>';
+    public function html()
+    {
+        echo '<h1>' . $this->getLang('name') . '</h1>';
         echo '<form action="doku.php" method="GET"><fieldset class="clearhistory">';
         echo '<input type="hidden" name="do" value="admin" />';
         echo '<input type="hidden" name="page" value="clearhistory" />';
 
         echo '<fieldset><input type="radio" name="clear" value="1" id="c1" checked="checked" /> <label for="c1">';
-        echo $this->getLang('clean on recent changes').'</label></fieldset><br/>';
+        echo $this->getLang('clean on recent changes') . '</label></fieldset><br/>';
 
         echo '<fieldset><input type="radio" name="clear" value="2" id="c2" /> <label for="c2">';
         echo $this->getLang('clean on namespace');
         echo '</label><br /><input type="text" name="ns" class="edit" />';
         echo '</fieldset><br/>';
-		echo '<fieldset>';
-		echo '<input type="checkbox" name="onlysmall" id="c3" '. ($this->getConf('autoclearonlysmall')?'checked="checked"':'') .'  /> <label for="c3">'.$this->getLang('onlysmall').'</label><br />';
-		echo '<input type="checkbox" name="onlynocomment" id="c4" '. ($this->getConf('autoclearonlynocomment')?'checked="checked"':'') .' /> <label for="c4">'.$this->getLang('onlynocomment').'</label><br />';
-		echo '</fieldset>';
+        echo '<fieldset>';
+        echo '<input type="checkbox" name="onlysmall" id="c3" ' . ($this->getConf('autoclearonlysmall') ? 'checked="checked"' : '') . '  /> <label for="c3">' . $this->getLang('onlysmall') . '</label><br />';
+        echo '<input type="checkbox" name="onlynocomment" id="c4" ' . ($this->getConf('autoclearonlynocomment') ? 'checked="checked"' : '') . ' /> <label for="c4">' . $this->getLang('onlynocomment') . '</label><br />';
+        echo '</fieldset>';
 
-        echo '<input type="submit" value="'.$this->getLang('do').'" class="button" /></fieldset>';
+        echo '<input type="submit" value="' . $this->getLang('do') . '" class="button" /></fieldset>';
         echo '</form>';
-        echo '<p class="clearhistory">'.$this->getLang('desctext').'</p>';
+        echo '<p class="clearhistory">' . $this->getLang('desctext') . '</p>';
     }
 
-	/**
-	 * Scans throu a namespace and count the deleted pages in $this->delcounter
-	 *
-     * @param string	$ns 			the namespace to search in
-	 * @param boolean	$onlySmall 		only delete small changes on true
-	 * @param boolean	$onlyNoComment	don't delete changes with a comment on true
-	 */
-	function _scanNamespace($ns, $onlySmall = false, $onlyNoComment = false) {
-		$this->delcounter = 0;
-		$this->_scan($ns, $onlySmall, $onlyNoComment);
-	}
-
+    /**
+     * Scans throu a namespace and count the deleted pages in $this->delcounter
+     *
+     * @param string $ns the namespace to search in
+     * @param boolean $onlySmall only delete small changes on true
+     * @param boolean $onlyNoComment don't delete changes with a comment on true
+     */
+    protected function _scanNamespace($ns, $onlySmall = false, $onlyNoComment = false)
+    {
+        $this->delcounter = 0;
+        $this->_scan($ns, $onlySmall, $onlyNoComment);
+    }
 
     /**
      * Scans namespaces for deletable revisions
-	 *
-     * @param string	$ns				the namespace to search in
-	 * @param boolean	$onlySmall 		only delete small changes on true
-	 * @param boolean	$onlyNoComment	don't delete changes with a comment on true
+     *
+     * @param string $ns the namespace to search in
+     * @param boolean $onlySmall only delete small changes on true
+     * @param boolean $onlyNoComment don't delete changes with a comment on true
      */
-    function _scan($ns = '', $onlySmall = false, $onlyNoComment = false) {
-        $dir = preg_replace('/\.txt(\.gz)?/i','', wikiFN($ns));
-        $dir = rtrim($dir,'/');
+    protected function _scan($ns = '', $onlySmall = false, $onlyNoComment = false)
+    {
+        $dir = preg_replace('/\.txt(\.gz)?/i', '', wikiFN($ns));
+        $dir = rtrim($dir, '/');
         if (!is_dir($dir)) return;
         $dh = opendir($dir);
         if (!$dh) {
@@ -125,15 +105,16 @@ class admin_plugin_clearhistory extends DokuWiki_Admin_Plugin {
             return;
         }
         while (($file = readdir($dh)) !== false) {
-            if ($file == '.' || $file == '..' ) continue;
-            if (is_dir($dir.'/'.$file)) {
-                $this->_scan($ns.':'.$file);
+            if ($file == '.' || $file == '..') continue;
+            if (is_dir($dir . '/' . $file)) {
+                $this->_scan($ns . ':' . $file);
                 continue;
             }
             if ($file[0] == '_') continue;
-            if (substr($file,-4) == '.txt') {
-                $name = substr($file,0,-4);
-                $this->_parseChangesFile(metaFN($ns.':'.$name,'.changes'),$ns.':'.$name, $onlySmall, $onlyNoComment);
+            if (substr($file, -4) == '.txt') {
+                $name = substr($file, 0, -4);
+                $this->_parseChangesFile(metaFN($ns . ':' . $name, '.changes'), $ns . ':' . $name, $onlySmall,
+                    $onlyNoComment);
             }
         }
         closedir($dh);
@@ -141,109 +122,93 @@ class admin_plugin_clearhistory extends DokuWiki_Admin_Plugin {
 
     /**
      * Scans the recent changed files for changes
-	 *
-     * @param int 		$num 			number of last changed files to scan
-	 * @param boolean	$onlySmall 		only delete small changes on true
-	 * @param boolean	$onlyNoComment	don't delete changes with a comment on true
+     *
+     * @param int $num number of last changed files to scan
+     * @param boolean $onlySmall only delete small changes on true
+     * @param boolean $onlyNoComment don't delete changes with a comment on true
      */
-    function _scanRecents( $num = 30, $onlySmall = false , $onlyNoComment = false ) {
-        $recents = getRecents(0,$num);
+    public function _scanRecents($num = 30, $onlySmall = false, $onlyNoComment = false)
+    {
+        $recents = getRecents(0, $num);
 
         $this->delcounter = 0;
         foreach ($recents as $recent) {
-            $this->_parseChangesFile(metaFN($recent['id'],'.changes'),$recent['id'], $onlySmall, $onlyNoComment);
+            $this->_parseChangesFile(metaFN($recent['id'], '.changes'), $recent['id'], $onlySmall, $onlyNoComment);
         }
     }
 
     /**
      * Parses a .changes file for deletable pages and deletes them.
-	 *
-     * @param string	$file			the path to the change file
-	 * @param string	$page			wiki pagename
-	 * @param boolean	$onlySmall		deletes only small changes
-	 * @param boolean	$onlyNoComment	deletes only entrys without a comment
+     *
+     * @param string $file the path to the change file
+     * @param string $page wiki pagename
+     * @param boolean $onlySmall deletes only small changes
+     * @param boolean $onlyNoComment deletes only entrys without a comment
      */
-    function _parseChangesFile( $file , $page , $onlySmall = false , $onlyNoComment = false ) {
+    protected function _parseChangesFile($file, $page, $onlySmall = false, $onlyNoComment = false)
+    {
         if (!is_file($file)) return;
         if (checklock($page)) return;
         lock($page);
         $content = file_get_contents($file);
         // get page informations
-        $max = preg_match_all('/^([0-9]+)\s+?([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+?(E|C|D|R)\s+?(\S+)\s+?(\S*)\s+?(.*)$/im',$content,$match);
+        $max = preg_match_all('/^([0-9]+)\s+?([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+?(E|C|D|R)\s+?(\S+)\s+?(\S*)\s+?(.*)$/im',
+            $content, $match);
         if ($max <= 1) return;
         // set mark to creation entry
-        $cmptime = $match[1][$i]+9999999999;
-        $cmpuser = (empty($match[5][$max-1]))?$match[2][$max-1]:$match[5][$max-1]; // user or if not logged in ip
+        $cmptime = $match[1][$i] + 9999999999;
+        $cmpuser = (empty($match[5][$max - 1])) ? $match[2][$max - 1] : $match[5][$max - 1]; // user or if not logged in ip
         $newcontent = '';
-        for ($i=$max-1;$i>=0;$i--) {
-            $user = (empty($match[5][$i]))?$match[2][$i]:$match[5][$i]; // user or if not logged in ip
+        for ($i = $max - 1; $i >= 0; $i--) {
+            $user = (empty($match[5][$i])) ? $match[2][$i] : $match[5][$i]; // user or if not logged in ip
             $time = $match[1][$i];
             // Creations arnt touched
             if ($match[3][$i] != "E" && $match[3][$i] != "e") {
                 $cmpuser = $user;
                 $cmptime = $time;
-                $newcontent = $this->_addLine($match,$i) . $newcontent;
+                $newcontent = $this->_addLine($match, $i) . $newcontent;
                 continue;
             }
             // if not the same user -> new mark and continue
             if ($user != $cmpuser) {
                 $cmpuser = $user;
                 $cmptime = $time;
-                $newcontent = $this->_addLine($match,$i) . $newcontent;
+                $newcontent = $this->_addLine($match, $i) . $newcontent;
                 continue;
             }
 
-			// if onlySmall is set we just want to handle the small matches
-			if ($onlySmall && $match[3][$i] == 'E') {
-				$cmpuser = $user;
-				$cmptime = $time;
-				$newcontent = $this->_addLine($match,$i) . $newcontent;
-				continue;
-			}
+            // if onlySmall is set we just want to handle the small matches
+            if ($onlySmall && $match[3][$i] == 'E') {
+                $cmpuser = $user;
+                $cmptime = $time;
+                $newcontent = $this->_addLine($match, $i) . $newcontent;
+                continue;
+            }
 
-			// if onlyNoComment is set we pass all lines with a comment
-			if ($onlyNoComment && trim($match[6][$i]) != '') {
-				$cmpuser = $user;
-				$cmptime = $time;
-				$newcontent = $this->_addLine($match,$i) . $newcontent;
-				continue;
-			}
+            // if onlyNoComment is set we pass all lines with a comment
+            if ($onlyNoComment && trim($match[6][$i]) != '') {
+                $cmpuser = $user;
+                $cmptime = $time;
+                $newcontent = $this->_addLine($match, $i) . $newcontent;
+                continue;
+            }
 
             // check the time difference between the entrys
-            if ( $cmptime-(60*60) < $time ) {
-                @unlink(wikiFN($match[4][$i],$time));
+            if ($cmptime - (60 * 60) < $time) {
+                @unlink(wikiFN($match[4][$i], $time));
                 $this->delcounter++;
                 continue;
             }
             $cmptime = $time;
-            $newcontent = $this->_addLine($match,$i) . $newcontent;
+            $newcontent = $this->_addLine($match, $i) . $newcontent;
         }
         unlock($page);
-        io_saveFile($file,$newcontent);
+        io_saveFile($file, $newcontent);
     }
 
-	function _addLine($match,$i) {
-		return $match[0][$i]."\n";
-	}
-
-	/**
-	 * shows that this function is accessible only by admins
-	 *
-	 * @return true
-	 */
-    function forAdminOnly() {
-        return true;
-    }
-
-	/**
-	 * returns the name in the menu
-	 *
-	 * @return Menu name for the plugin
-	 */
-    function getMenuText($lang) {
-        return $this->getLang('menu');
+    protected function _addLine($match, $i)
+    {
+        return $match[0][$i] . "\n";
     }
 
 }
-
-?>
